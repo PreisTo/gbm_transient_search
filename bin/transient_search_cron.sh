ccab5ea729783fe18ef2b96bc33f12e02e3778e8
Tobias Preis <tobias.preis@tum.de>
2023-08-02 14:26:50 +0200

    check if data available

--- bin/transient_search_cron.sh
+++ bin/transient_search_cron.sh
@@ -6,5 +6,5 @@
 #source /home/balrog/.cron_vars
 source /home/balrog/.venv/transient_search/bin/activate
 
-/home/balrog/.venv/transient_search/bin/python -m luigi --workers 32 --scheduler-host localhost --scheduler-port 8666 --module gbm_transient_search CreateReportDate --date $(date --date='-1 day' +'%Y-%m-%d') --remote-host raven;
+/home/balrog/sw/gbm_transient_search/bin/data_available && /home/balrog/.venv/transient_search/bin/python -m luigi --workers 32 --scheduler-host localhost --scheduler-port 8666 --module gbm_transient_search CreateReportDate --date $(date --date='-1 day' +'%Y-%m-%d') --remote-host raven;
 mv /home/balrog/logs/log.txt /home/balrog/logs/archive/log_$(date --date='-1 day' +'%y%m%d');
