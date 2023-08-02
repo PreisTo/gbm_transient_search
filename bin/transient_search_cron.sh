90105355c35e252b1e54e2a55e6f67c19082656a
Tobias Preis <tpreis@mpe.mpg.de>
2023-08-02 14:34:48 +0200

    fixed cron

--- bin/transient_search_cron.sh
+++ bin/transient_search_cron.sh
@@ -4,7 +4,8 @@
 
 #/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --workers 32 --module gbm_transient_search CreateReportDate --date $(date --date='-1 day' +'%Y-%m-%d')
 #source /home/balrog/.cron_vars
-source /home/balrog/.venv/transient_search/bin/activate
+#source /home/balrog/.venv/transient_search/bin/activate
 
-/home/balrog/sw/gbm_transient_search/bin/data_available && /home/balrog/.venv/transient_search/bin/python -m luigi --workers 32 --scheduler-host localhost --scheduler-port 8666 --module gbm_transient_search CreateReportDate --date $(date --date='-1 day' +'%Y-%m-%d') --remote-host raven;
+/home/balrog/.venv/transient_search/bin/python /home/balrog/sw/gbm_transient_search/bin/data_available --date $(date --date='-1 day' +'%y%m%d')
+/home/balrog/.venv/transient_search/bin/python -m luigi --workers 32 --scheduler-host localhost --scheduler-port 8666 --module gbm_transient_search CreateReportDate --date $(date --date='-1 day' +'%Y-%m-%d') --remote-host raven;
 mv /home/balrog/logs/log.txt /home/balrog/logs/archive/log_$(date --date='-1 day' +'%y%m%d');
